c36cd3cb063f8381e97c08a7b96951f9e8181816
Amar Yassa <amaryassa@yahoo.fr>
2017-11-07 23:30:04 +0100

    Creation des Methodes 1)Creer un cercle. 2)creer une ligne

--- src/theoriedesgraphesm1informatiqueamaryassa/TheorieDesGraphesM1InformatiqueAmarYassa.java
+++ src/theoriedesgraphesm1informatiqueamaryassa/TheorieDesGraphesM1InformatiqueAmarYassa.java
@@ -5,12 +5,17 @@
  */
 package theoriedesgraphesm1informatiqueamaryassa;
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 /**
@@ -18,28 +23,30 @@ import javafx.stage.Stage;
  * @author AMAR
  */
 public class TheorieDesGraphesM1InformatiqueAmarYassa extends Application {
+     private Circle createCircle(double x, double y, double r, Color color){
+     Circle circle = new Circle(x, y, r, color);   
+     return circle;
+     }
+     
+     private Line connect(Circle c1, Circle c2){
+        Line line = new Line();
+         
+        return line;
+     }
+  @Override
+  public void start(Stage primaryStage) {
+    Group root = new Group();
+    Scene scene = new Scene(root, 500, 260);
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+    //Fichier CSS 
+    final URL cssURL = getClass().getResource("style.css"); 
+    scene.getStylesheets().add(cssURL.toExternalForm());
+     
+
+      primaryStage.setScene(scene);
+    
+    primaryStage.show();
+  } 
 
     /**
      * @param args the command line arguments
