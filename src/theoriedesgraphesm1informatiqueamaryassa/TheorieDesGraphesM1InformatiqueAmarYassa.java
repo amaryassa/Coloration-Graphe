624f1a828791e53633a22777c9fc57874c44c0c7
Amar Yassa <amaryassa@yahoo.fr>
2017-11-08 01:52:00 +0100

    MEttre en place la méthode qui créer le cercle grace à un clique sur un bouton

--- src/theoriedesgraphesm1informatiqueamaryassa/TheorieDesGraphesM1InformatiqueAmarYassa.java
+++ src/theoriedesgraphesm1informatiqueamaryassa/TheorieDesGraphesM1InformatiqueAmarYassa.java
@@ -6,12 +6,17 @@
 package theoriedesgraphesm1informatiqueamaryassa;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -22,9 +27,40 @@ import javafx.stage.Stage;
  *
  * @author AMAR
  */
+
 public class TheorieDesGraphesM1InformatiqueAmarYassa extends Application {
+    double orgSceneX, orgSceneY;
+    int a=0;
+    Map<Integer,Circle> CircleMap = new HashMap<Integer,Circle>();
+    
      private Circle createCircle(double x, double y, double r, Color color){
-     Circle circle = new Circle(x, y, r, color);   
+     Circle circle = new Circle(x, y, r, color);
+    
+     CircleMap.put(a, circle);
+     a++;
+ 
+
+    circle.setCursor(Cursor.HAND);
+
+    circle.setOnMousePressed((t) -> {
+      orgSceneX = t.getSceneX();
+      orgSceneY = t.getSceneY();
+
+      Circle c = (Circle) (t.getSource());
+      c.toFront();
+    });
+    circle.setOnMouseDragged((t) -> {
+      double offsetX = t.getSceneX() - orgSceneX;
+      double offsetY = t.getSceneY() - orgSceneY;
+
+      Circle c = (Circle) (t.getSource());
+
+      c.setCenterX(c.getCenterX() + offsetX);
+      c.setCenterY(c.getCenterY() + offsetY);
+
+      orgSceneX = t.getSceneX();
+      orgSceneY = t.getSceneY();
+    });  
      return circle;
      }
      
@@ -38,13 +74,34 @@ public class TheorieDesGraphesM1InformatiqueAmarYassa extends Application {
     Group root = new Group();
     Scene scene = new Scene(root, 500, 260);
     
-    //Fichier CSS 
+    //appelle au Fichier CSS 
     final URL cssURL = getClass().getResource("style.css"); 
     scene.getStylesheets().add(cssURL.toExternalForm());
-     
+     Pane paneGauche = new Pane();
+    paneGauche.setPrefSize(100, 260);
+    paneGauche.getStyleClass().add("paneGauche");
 
-      primaryStage.setScene(scene);
-    
+ Button b1 = new Button("B1");
+    b1.setId("b1");
+    b1.getStyleClass().add("b1");
+     b1.setOnAction(new EventHandler<ActionEvent>() { 
+            @Override
+            public void handle(ActionEvent event) {
+               
+                int MinX=100,MaxX=350,MinY=30,MaxY=300;
+                int X = MinX + (int)(Math.random() * ((MaxX - MinX) + 1));
+                int Y = MinY + (int)(Math.random() * ((MaxY - MinY) + 1));
+                Circle cercle = createCircle(X, Y, 30, Color.RED);
+                root.getChildren().add(cercle);
+                cercle.toFront();
+                
+                System.out.println(" Lala!"+CircleMap.get(0));
+                
+            }
+        });
+    paneGauche.getChildren().add(b1);
+    root.getChildren().add(paneGauche);
+    primaryStage.setScene(scene); 
     primaryStage.show();
   } 
 
